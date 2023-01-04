de97bfa41ddef7b009e7b7ea7bf44a38a1739339
waclove <waclove@yandex.ru>
2023-01-04 22:56:06 +0300

    'rep'

--- .all/make_gif_from_folder.py
+++
@@ -1,9 +0,0 @@
-import imageio
-import glob
-
-def make_gif(dir_path, gifname, fps=20):
-    filenames = [_ for _ in glob.glob(f'{dir_path}/*.png')]
-    images = []
-    for filename in filenames:
-        images.append(imageio.imread(filename))
-    imageio.mimsave(f'{dir_path}/{gifname}.gif', images, format='gif', fps=fps)
